3f44ec4475a896a690036c705b0b4799e9621245
Janiss Binder <binder@puzzle.ch>
2018-11-23 09:13:09 +0100

    Adjust airbrake config to new version

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -7,19 +7,24 @@
 
 Airbrake.configure do |config|
   # if no host is given, use airbrake only in test mode
-  config.test_mode   = ENV['RAILS_AIRBRAKE_HOST'].blank?
-
-  config.api_key     = ENV['RAILS_AIRBRAKE_API_KEY']
+  config.project_key = ENV['RAILS_AIRBRAKE_API_KEY']
+  config.project_id  = '42' # needs to be set to anything when using with errbit
   config.host        = ENV['RAILS_AIRBRAKE_HOST']
-  config.port        = (ENV['RAILS_AIRBRAKE_PORT'] || 443).to_i
-  config.secure      = config.port == 443
-  config.ignore         << ActionController::MethodNotAllowed
-  config.ignore         << ActionController::RoutingError
-  config.ignore         << ActionController::UnknownHttpMethod
-  config.params_filters << 'RAILS_DB_PASSWORD'
-  config.params_filters << 'RAILS_MAIL_RETRIEVER_PASSWORD'
-  config.params_filters << 'RAILS_AIRBRAKE_API_KEY'
-  config.params_filters << 'RAILS_SECRET_TOKEN'
-  config.params_filters << 'RAILS_MAIL_RETRIEVER_CONFIG'
-  config.params_filters << 'RAILS_MAIL_DELIVERY_CONFIG'
+
+  config.environment = Rails.env
+  config.ignore_environments = %w(development test)
+end
+
+Airbrake.add_filter do |notice|
+  notice.ignore! if notice.stash[:exception].is_a?(ActionController::MethodNotAllowed)
+  notice.ignore! if notice.stash[:exception].is_a?(ActionController::RoutingError)
+  notice.ignore! if notice.stash[:exception].is_a?(ActionController::UnknownHttpMethod)
+
+  %w(RAILS_DB_PASSWORD RAILS_MAIL_RETRIEVER_PASSWORD RAILS_AIRBRAKE_API_KEY
+  RAILS_SECRET_TOKEN RAILS_MAIL_RETRIEVER_CONFIG RAILS_MAIL_DELIVERY_CONFIG).each do |param|
+    if notice[:params][param]
+      # filter out param
+      notice[:params][param] = '[Filtered]'
+    end
+  end
 end
