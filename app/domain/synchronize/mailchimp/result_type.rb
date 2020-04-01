73460d0d5f7f0a21e8b545dd96c0d138faabdcd1
Andreas Maierhofer <maierhofer@puzzle.ch>
2020-04-01 17:47:54 +0200

    Do not rely on local constant lookup

--- app/domain/synchronize/mailchimp/result_type.rb
+++ app/domain/synchronize/mailchimp/result_type.rb
@@ -10,10 +10,10 @@ module Synchronize
       # from user or db
       def cast(value)
         case value
-        when String then Result.new(JSON.parse(value))
-        when Hash then Result.new(value)
-        when Result then value
-        else Result.new
+        when String then Synchronize::Mailchimp::Result.new(JSON.parse(value))
+        when Hash then Synchronize::Mailchimp::Result.new(value)
+        when Synchronize::Mailchimp::Result then value
+        else Synchronize::Mailchimp::Result.new
         end
       end
 
